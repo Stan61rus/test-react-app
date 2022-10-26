a7b2c0857b2d77cdb61bf576e32e49a424b8c5f9
k.polikanin <k.polikanin@bars-open.ru>
2022-10-26 15:54:29 +0300

    Create new react component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import React, { useState } from "react";
+import Counter from "./component/Counter";
 
 function App() {
-  const [count, setCount] = useState(5);
-  const [text, setText] = useState("Hello world!");
-
   return (
     <div className="App">
-      <h1>{count}</h1>
-      <h1>{text}</h1>
-
-      <input
-        type="text"
-        value={text}
-        onChange={(event) => setText(event.target.value)}
-      ></input>
-      <button onClick={() => setCount(count + 1)}>Прибавить</button>
-      <button onClick={() => setCount(count - 1)}>Отнять</button>
+      <Counter />
+      <Counter />
     </div>
   );
 }
