0312327105495446c61f48942f6896a5c26ff6e4
k.polikanin <k.polikanin@bars-open.ru>
2022-10-26 12:58:33 +0300

    change App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return React.createElement(
-    "button",
-    { disabled: false, onClick: () => console.log("test") },
-    "Test"
+  const [count, setCount] = useState(5);
+  const [text, setText] = useState("Hello world!");
+
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <h1>{text}</h1>
+
+      <input
+        type="text"
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+      ></input>
+      <button onClick={() => setCount(count + 1)}>Прибавить</button>
+      <button onClick={() => setCount(count - 1)}>Отнять</button>
+    </div>
   );
 }
 
