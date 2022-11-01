5364b4b6f8156851fabd5c0a1549d9c9f5441bb7
k.polikanin <k.polikanin@bars-open.ru>
2022-11-01 17:53:28 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PostList from "./component/PostList";
+import MyButton from "./component/UI/button/MyButton";
 import "./styles/App.css";
 
 function App() {
@@ -9,16 +10,14 @@ function App() {
     { id: 3, title: "JavaScript 3", body: "Description" },
   ];
 
-  const posts2 = [
-    { id: 1, title: "Pyton", body: "Description" },
-    { id: 2, title: "Pyton 2", body: "Description" },
-    { id: 3, title: "Pyton 3", body: "Description" },
-  ];
-
   return (
     <div className="App">
+      <form>
+        <input type="text" placeholder="Введите заголовок" />
+        <input type="text" placeholder="Введите описание" />
+        <MyButton>Создать пост</MyButton>
+      </form>
       <PostList posts={posts} title="Посты про JavaScript" />
-      <PostList posts={posts2} title="Посты про Pyton" />
     </div>
   );
 }
