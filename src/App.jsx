f978ec8d4dca0abf9db772488d043bcab39f3ae0
Stan61rus <37905051+Stan61rus@users.noreply.github.com>
2022-11-17 16:35:34 +0300

    добавил управляемый input

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,26 @@ import React, { useState } from "react";
 
 function App() {
   const [likes, setLikes] = useState(0);
+  const [input, setInput] = useState("test");
 
-  function decrement(params) {
+  function decrement() {
     setLikes(likes + 1);
   }
 
-  function increment(params) {
+  function increment() {
     setLikes(likes + 1);
   }
 
   return (
     <div className="APP">
+      <h1>{input}</h1>
       <h1>{likes}</h1>
+
+      <input
+        type="text"
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      />
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
     </div>
