4af73bd1127fcae3fcf98d6f23686681e72dc5d7
Stan61rus <37905051+Stan61rus@users.noreply.github.com>
2022-11-18 15:25:12 +0300

    Добавил компонент посто

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
-import React, { useState } from "react";
 import Counter from "./components/counter";
 import ClassCounter from "./components/classCounter";
 import Input from "./components/input";
+import PostItem from "./components/postItem";
+
 import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="post">
-        <div className="post__content">
-          <strong>1. Javascript</strong>
-          <div>Javascript - язык программирования</div>
-        </div>
-        <div className="post__btns">
-          <button>Удалить</button>
-        </div>
-      </div>
-
-      <br />
+      <PostItem />
       <Counter />
       <ClassCounter />
       <Input />
